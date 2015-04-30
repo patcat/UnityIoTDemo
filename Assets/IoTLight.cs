4818c2b8e694cda70775ce577310001bdac06b43
Patrick Catanzariti <thatpatrickguy@gmail.com>
2015-04-30 18:26:33 +1000

    Initial commit

---
+++ Assets/IoTLight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IoTLight : MonoBehaviour {
+	public string token = "9c0f99363411f0fd2c650ce1bbd8c0a5a3d4cd2e";
+	public string deviceId = "55ff6a065075555341101787";
+	public Light sceneLight;
+
+	IEnumerator AdjustLightWithSensor() {
+		while (true) {
+			string lightUrl = "https://api.spark.io/v1/devices/" + deviceId + "/light?access_token=" + token;
+			
+			WWW lightWWW = new WWW (lightUrl);
+			yield return lightWWW;
+			Debug.Log (lightWWW.text);
+			JSONObject lightData = new JSONObject (lightWWW.text);
+			
+			float light = lightData ["result"].n;
+			
+			sceneLight.intensity = light / 255;
+			
+			yield return new WaitForSeconds (10);
+		}
+	}
+
+	void Start () {
+		sceneLight = GetComponent<Light>();
+		
+		StartCoroutine (AdjustLightWithSensor());
+	}
+}
---
+++ Assets/IoTSkybox.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IoTSkybox : MonoBehaviour {
+	public Material clearSky;
+	public Material cloudySky;
+	
+	IEnumerator AdjustSkyToWeather() {
+		while (true) {
+			string weatherUrl = "http://api.openweathermap.org/data/2.5/weather?zip=2000,au";
+			
+			WWW weatherWWW = new WWW (weatherUrl);
+			yield return weatherWWW;
+			Debug.Log (weatherWWW.text);
+			JSONObject tempData = new JSONObject (weatherWWW.text);
+			
+			JSONObject weatherDetails = tempData["weather"];
+			string WeatherType = weatherDetails[0]["main"].str;
+			Debug.Log (WeatherType);
+			
+			if (WeatherType == "Clear") {
+				RenderSettings.skybox = clearSky;
+			} else if (WeatherType == "Clouds" || WeatherType == "Rain") {
+				RenderSettings.skybox = cloudySky;
+			}
+			
+			yield return new WaitForSeconds(60);
+		}
+	}
+	
+	void Start () {
+		StartCoroutine (AdjustSkyToWeather());
+	}
+}
